579c30c30cf0f6801807be5039a23a3225976fce
alexadam <paddze21@gmail.com>
2020-02-13 19:05:40 -0500

    Added most recent versions of notebookswq

--- utils.py
+++ utils.py
@@ -42,7 +42,7 @@ def update_model_no_feedback(model, x_update, y_update, x_test, y_test, num_upda
     return new_model, rates
 
 
-def update_model_feedback(model, x_update, y_update, x_test, y_test, num_updates, intermediate=False):
+def update_model_feedback(model, x_update, y_update, x_test, y_test, num_updates, intermediate=False, threshold=None):
     np.random.seed(1)
     new_model = copy.deepcopy(model)
     
@@ -65,13 +65,18 @@ def update_model_feedback(model, x_update, y_update, x_test, y_test, num_updates
         new_model.partial_fit(sub_x, sub_y, classes)
         
         if intermediate:
-            new_pred = new_model.predict(x_test)
+            if threshold is None:
+                new_pred = new_model.predict(x_test)
+            else:
+                pred_prob = new_model.predict_proba(x_test)
+                new_pred = pred_prob[:, 1] >= threshold
+            
             updated_tnr, updated_fpr, updated_fnr, updated_tpr = eval_model(y_test, new_pred)
             rates["fpr"].append(updated_fpr)
             rates["tpr"].append(updated_tpr)
             rates["fnr"].append(updated_fnr)
             rates["tnr"].append(updated_tnr)
-        
+                
     return new_model, rates
 
 
@@ -100,58 +105,34 @@ def update_model_noise(model, x, y, num_updates, rate):
     return new_model
 
 
-def update_model_constant_trust(model, x, y, num_updates, trust, physician_fpr):
+def update_model_conditional_trust(model, x_update, y_update, x_test, y_test, num_updates, physician_fpr, intermediate=False):
     np.random.seed(1)
     new_model = copy.deepcopy(model)
     
-    size = float(len(y)) / float(num_updates)
+    size = float(len(y_update)) / float(num_updates)
 
-    classes = np.unique(y)
+    classes = np.unique(y_update)
+    
+    trust = None
+    
+    trusts = []
+    rates = {"fpr": [], "tpr": [], "fnr": [], "tnr": []}
 
     for i in range(num_updates):
         idx_start = int(size * i)
         idx_end = int(size * (i + 1))
-        sub_x = x[idx_start: idx_end, :]
-        sub_y = copy.deepcopy(y[idx_start: idx_end])
+        sub_x = x_update[idx_start: idx_end, :]
+        sub_y = copy.deepcopy(y_update[idx_start: idx_end])
 
         model_pred = new_model.predict(sub_x)
         model_fp_idx = np.where(np.logical_and(sub_y == 0, model_pred == 1))[0]
         model_pred = copy.deepcopy(sub_y)
         model_pred[model_fp_idx] = 1
         
-        physician_pred = copy.deepcopy(sub_y)        
-        neg_idx = np.where(physician_pred == 0)[0]
-        physician_fp_idx = np.random.choice(neg_idx, int(physician_fpr * len(sub_y)))
-        physician_pred[physician_fp_idx] = 1
+        if trust is None:
+            trust = 1 - float(len(model_fp_idx)) / float(len(sub_y))
         
-        bernoulli = np.random.choice([0, 1], len(sub_y), p=[1 - trust, trust])
-        
-        target = bernoulli * model_pred + (1 - bernoulli) * physician_pred
-
-        new_model.partial_fit(sub_x, target, classes)
-        
-    return new_model
-
-
-def update_model_conditional_trust(model, x, y, num_updates, initial_trust, physician_fpr):
-    np.random.seed(1)
-    new_model = copy.deepcopy(model)
-    
-    size = float(len(y)) / float(num_updates)
-
-    classes = np.unique(y)
-    trust = initial_trust
-
-    for i in range(num_updates):
-        idx_start = int(size * i)
-        idx_end = int(size * (i + 1))
-        sub_x = x[idx_start: idx_end, :]
-        sub_y = copy.deepcopy(y[idx_start: idx_end])
-
-        model_pred = new_model.predict(sub_x)
-        model_fp_idx = np.where(np.logical_and(sub_y == 0, model_pred == 1))[0]
-        model_pred = copy.deepcopy(sub_y)
-        model_pred[model_fp_idx] = 1
+        trusts.append(trust)
         
         physician_pred = copy.deepcopy(sub_y)        
         neg_idx = np.where(physician_pred == 0)[0]
@@ -168,48 +149,17 @@ def update_model_conditional_trust(model, x, y, num_updates, initial_trust, phys
         
         fpr = float(len(model_fp_idx)) / float(len(sub_y))
         
-        trust = 1 - 2 * fpr
+        if intermediate:
+            new_pred = new_model.predict(x_test)          
+            updated_tnr, updated_fpr, updated_fnr, updated_tpr = eval_model(y_test, new_pred)
+            rates["fpr"].append(updated_fpr)
+            rates["tpr"].append(updated_tpr)
+            rates["fnr"].append(updated_fnr)
+            rates["tnr"].append(updated_tnr)
         
-    return new_model
-
-
-def update_model_monotonically_increasing_trust(model, x, y, num_updates, initial_trust, physician_fpr):
-    np.random.seed(1)
-    new_model = copy.deepcopy(model)
-    
-    size = float(len(y)) / float(num_updates)
-
-    classes = np.unique(y)
-    
-    trusts = np.linspace(initial_trust, 1.0, num_updates)
-
-    for i in range(num_updates):
-        trust = trusts[i]
+        trust = 1 - fpr
         
-        idx_start = int(size * i)
-        idx_end = int(size * (i + 1))
-        sub_x = x[idx_start: idx_end, :]
-        sub_y = copy.deepcopy(y[idx_start: idx_end])
-
-        model_pred = new_model.predict(sub_x)
-        model_fp_idx = np.where(np.logical_and(sub_y == 0, model_pred == 1))[0]
-        model_pred = copy.deepcopy(sub_y)
-        model_pred[model_fp_idx] = 1
-        
-        physician_pred = copy.deepcopy(sub_y)        
-        neg_idx = np.where(physician_pred == 0)[0]
-        physician_fp_idx = np.random.choice(neg_idx, int(physician_fpr * len(sub_y)))
-        physician_pred[physician_fp_idx] = 1
-        
-        bernoulli = np.random.choice([0, 1], len(sub_y), p=[1 - trust, trust])
-        
-        target = bernoulli * model_pred + (1 - bernoulli) * physician_pred
-
-        new_model.partial_fit(sub_x, target, classes)
-        model_pred = new_model.predict(sub_x)
-        model_fp_idx = np.where(np.logical_and(sub_y == 0, model_pred == 1))[0]
-        
-    return new_model
+    return new_model, rates, trusts
 
 
 def update_model_increasing_trust(model, x, y, num_updates, trusts, physician_fpr):
@@ -235,8 +185,10 @@ def update_model_increasing_trust(model, x, y, num_updates, trusts, physician_fp
         
         physician_pred = copy.deepcopy(sub_y)        
         neg_idx = np.where(physician_pred == 0)[0]
-        physician_fp_idx = np.random.choice(neg_idx, int(physician_fpr * len(sub_y)))
-        physician_pred[physician_fp_idx] = 1
+        
+        if len(neg_idx) > 0:
+            physician_fp_idx = np.random.choice(neg_idx, min(len(neg_idx), int(physician_fpr * len(sub_y))))
+            physician_pred[physician_fp_idx] = 1
         
         bernoulli = np.random.choice([0, 1], len(sub_y), p=[1 - trust, trust])
         
@@ -324,7 +276,7 @@ def update_model_feedback_with_training_cumulative(model, x_train, y_train, x_up
     return new_model, rates
 
 
-def update_model_full_fit(model, x_train, y_train, x_update, y_update, num_updates):
+def update_model_full_fit_feedback(model, x_train, y_train, x_update, y_update, num_updates):
     np.random.seed(1)
     new_model = copy.deepcopy(model)
     
@@ -352,10 +304,37 @@ def update_model_full_fit(model, x_train, y_train, x_update, y_update, num_updat
         
         new_model.fit(np.concatenate((cumulative_x, x_train)), np.concatenate((cumulative_y, y_train)))
         
-    return new_model
+    return new_model, None
 
 
-def update_model_feedback_linear_trend(model, x_update, y_update, num_updates, offset):
+def update_model_full_fit_no_feedback(model, x_train, y_train, x_update, y_update, num_updates):
+    np.random.seed(1)
+    new_model = copy.deepcopy(model)
+    
+    size = float(len(y_update)) / float(num_updates)
+    
+    cumulative_x = None
+    cumulative_y = None
+
+    for i in range(num_updates):
+        idx_start = int(size * i)
+        idx_end = int(size * (i + 1))
+        sub_x = x_update[idx_start: idx_end, :]
+        sub_y = copy.deepcopy(y_update[idx_start: idx_end])
+
+        if cumulative_x is None:
+            cumulative_x = sub_x
+            cumulative_y = sub_y
+        else:
+            cumulative_x = np.concatenate((cumulative_x, sub_x))
+            cumulative_y = np.concatenate((cumulative_y, sub_y))
+        
+        new_model.fit(np.concatenate((cumulative_x, x_train)), np.concatenate((cumulative_y, y_train)))
+        
+    return new_model, None
+
+
+def update_model_feedback_linear_trend(model, x_update, y_update, x_test, y_test, num_updates, offset, intermediate=False):
     np.random.seed(1)
     new_model = copy.deepcopy(model)
     
@@ -363,6 +342,8 @@ def update_model_feedback_linear_trend(model, x_update, y_update, num_updates, o
 
     classes = np.unique(y_update)
     offsets = np.linspace(0, offset, num_updates)
+    
+    rates = {"fpr": [], "tpr": [], "fnr": [], "tnr": []}
 
     for i in range(num_updates):
         idx_start = int(size * i)
@@ -376,7 +357,15 @@ def update_model_feedback_linear_trend(model, x_update, y_update, num_updates, o
 
         new_model.partial_fit(sub_x, sub_y, classes)
         
-    return new_model
+        if intermediate:
+            new_pred = new_model.predict(x_test)          
+            updated_tnr, updated_fpr, updated_fnr, updated_tpr = eval_model(y_test, new_pred)
+            rates["fpr"].append(updated_fpr)
+            rates["tpr"].append(updated_tpr)
+            rates["fnr"].append(updated_fnr)
+            rates["tnr"].append(updated_tnr)
+        
+    return new_model, rates
 
 
 def perturb_labels_fp(y, rate=0.05):
@@ -393,12 +382,19 @@ def perturb_labels_fp(y, rate=0.05):
     return y_copy
 
 
-def make_gaussian_data(m0, m1, s0, s1, n, p0, p1, features=2):
+def make_gaussian_data(m0, m1, s0, s1, n, p0, p1, features=2, noise=0.0):
     neg_samples = np.random.multivariate_normal(m0 * np.ones(features), s0 * np.eye(features), int(n * p0))
     pos_samples = np.random.multivariate_normal(m1 * np.ones(features), s1 * np.eye(features), int(n * p1))
     
     x = np.concatenate((neg_samples, pos_samples))
     y = np.concatenate((np.zeros(len(neg_samples)), np.ones(len(pos_samples))))
+    
+    if noise > 0.0:
+        neg_idx = y == 0
+        bernoulli = np.random.choice([False, True], len(y), p=[1 - noise, noise])
+        neg_idx = np.logical_and(neg_idx, bernoulli)
+        neg_idx = np.where(neg_idx)
+        y[neg_idx] = 1
     
     idx = np.random.choice(len(x), len(x), replace=False)
     x = x[idx]
@@ -427,3 +423,34 @@ def make_trend_gaussian_data(m0, m1, s0, s1, n, features=2, uniform_range=[-3, 3
     
     return x, y
 
+
+
+def load_mimiciii_data():
+    df_adult = pd.read_csv('adult_icu.gz', compression='gzip')
+    
+    train_cols = [
+    'age', 'first_hosp_stay', 'first_icu_stay', 'eth_asian',
+    'eth_black', 'eth_hispanic', 'eth_other', 'eth_white',
+    'admType_ELECTIVE', 'admType_EMERGENCY', 'admType_NEWBORN',
+    'admType_URGENT', 'heartrate_min', 'heartrate_max', 'heartrate_mean',
+    'sysbp_min', 'sysbp_max', 'sysbp_mean', 'diasbp_min', 'diasbp_max',
+    'diasbp_mean', 'meanbp_min', 'meanbp_max', 'meanbp_mean',
+    'resprate_min', 'resprate_max', 'resprate_mean', 'tempc_min',
+    'tempc_max', 'tempc_mean', 'spo2_min', 'spo2_max', 'spo2_mean',
+    'glucose_min', 'glucose_max', 'glucose_mean', 'aniongap', 'albumin',
+    'bicarbonate', 'bilirubin', 'creatinine', 'chloride', 'glucose',
+    'hematocrit', 'hemoglobin', 'lactate', 'magnesium', 'phosphate',
+    'platelet', 'potassium', 'ptt', 'inr', 'pt', 'sodium', 'bun', 'wbc']
+    
+    label = 'mort_icu'
+    X_df = df_adult[train_cols]
+    y_df = df_adult[label]
+    
+    dataset = {
+    'problem': 'classification',
+    'X': X_df,
+    'y': y_df,
+    'd_name': 'mimiciii',
+    }
+    
+    return X_df, y_df
