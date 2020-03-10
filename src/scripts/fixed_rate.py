3ab1f86dea2a1a4da179cb7a88d33845de5db008
alexadam <paddze21@gmail.com>
2020-03-10 12:13:48 -0400

    Added script to find models with config
    -Changed legend position in plots for src/scripts/fixed_rate.py

--- src/scripts/fixed_rate.py
+++ src/scripts/fixed_rate.py
@@ -22,31 +22,31 @@ from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_sco
 
 parser = ArgumentParser()
 parser.add_argument("--data-type", default="mimic", choices=["mimic", "support2", "gaussian"], type=str)
-parser.add_argument("--seeds", default=5, type=int)
+parser.add_argument("--seeds", default=10, type=int)
 parser.add_argument("--model", default="nn", type=str)
 
 parser.add_argument("--n-train", default=0.1, type=percentage)
 parser.add_argument("--n-update", default=0.7, type=percentage)
 parser.add_argument("--n-test", default=0.2, type=percentage)
-parser.add_argument("--num-updates", default=100, type=int)
+parser.add_argument("--num-updates", default=500, type=int)
 parser.add_argument("--num-features", default=20, type=int)
 
 parser.add_argument("--initial-desired-rate", default="fpr", type=str)
 parser.add_argument("--initial-desired-value", default=0.1, type=float)
 
-parser.add_argument("--dynamic-desired-rate", default=None, type=str)
+parser.add_argument("--dynamic-desired-rate", default="fpr", type=str)
 
-parser.add_argument("--rate-types", default=["auc", "fpr", "fnr"], nargs="+")
+parser.add_argument("--rate-types", default=["auc", "fpr", "fnr", "tnr"], nargs="+")
 
 parser.add_argument("--lr", default=0.1, type=float)
-parser.add_argument("--iterations", default=1000, type=int)
+parser.add_argument("--iterations", default=250, type=int)
 parser.add_argument("--importance", default=100000.0, type=float)
 
-parser.add_argument("--hidden-layers", default=1, type=int)
+parser.add_argument("--hidden-layers", default=0, type=int)
 parser.add_argument("--activation", default="Tanh", type=str)
 
 parser.add_argument("--bad-model", default=False, type=str2bool)
-parser.add_argument("--update-type", default="no_feedback", type=str)
+parser.add_argument("--update-type", default="feedback", type=str)
 
 
 def train_update_loop(model_fn, n_train, n_update, n_test, num_updates, num_features,
@@ -165,18 +165,20 @@ def summarize_stats(stats):
 
 
 def plot_rates(data, rate_types, gs_lines, title, plot_path):
-    fig = plt.figure(figsize=(13, 9))
+    fig = plt.figure(figsize=(13, 13))
     ax = fig.add_subplot(111)
     g = sns.lineplot(x="num_updates", y="rate", hue="type", data=data.loc[data["type"].isin(rate_types)],
                      err_style="band", ax=ax, ci="sd", palette="bright")
 
     ax.set_xlabel("Num Updates", size=30, labelpad=10.0)
     ax.set_ylabel("Rate", size=30, labelpad=10.0)
+    labels = []
 
     for i in range(len(g.lines)):
         label = g.lines[i].get_label()
 
         if label in gs_lines.keys():
+            labels.append(label.upper())
             ax.axhline(gs_lines[label], ls="--", color=g.lines[i].get_color())
 
     ax.set_xlim([0, 500])
@@ -186,7 +188,9 @@ def plot_rates(data, rate_types, gs_lines, title, plot_path):
 
     fig.suptitle(title)
 
-    legend = ax.legend(title="Rate Type", title_fontsize=30,
+    # legend = ax.legend(title="Rate Type", labels=labels, title_fontsize=30,
+    #                    loc="upper right", bbox_to_anchor=(1.25, 1), borderaxespad=0.)
+    legend = ax.legend(title="Rate Type", labels=labels, title_fontsize=30,
                        loc="upper right")
     legend.texts[0].set_size(24)
 
