914a50cb04ebecf1766e6ea24e5bcd0ff31df091
alexadam <paddze21@gmail.com>
2020-10-26 19:16:17 -0400

    Added s-shaped dataset

--- src/utils/data.py
+++ src/utils/data.py
@@ -134,15 +134,15 @@ def generate_sklearn_make_classification_dataset(noise: float=0.0) -> Callable:
 
     return wrapped
 
-def generate_moons_dataset(n_train: int, n_update: int, n_test: int, num_features: int=2, noise: float=0.0) -> Tuple:
-    x_train, y_train = make_moons(n_train, noise=noise)
-    x_update, y_update = make_moons(n_update, noise=noise)
-    x_test, y_test = make_moons(n_test, noise=noise)
-    #     x, y = make_moons(n_train + n_update + n_test, noise=noise)
-    #     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=n_update + n_test)
-    #     x_update, x_test, y_update, y_test = train_test_split(x_test, y_test, test_size=n_test)
+def generate_moons_dataset(noise: float=0.0) -> Callable:
+    def wrapped(n_train: int, n_update: int, n_test: int, num_features: int=2) -> Tuple:
+        x_train, y_train = make_moons(n_train, noise=noise)
+        x_update, y_update = make_moons(n_update, noise=noise)
+        x_test, y_test = make_moons(n_test, noise=noise)
 
-    return x_train, y_train, x_update, y_update, x_test, y_test
+        return x_train, y_train, x_update, y_update, x_test, y_test
+
+    return wrapped
 
 
 def generate_circles_dataset(n_train: int, n_update: int, n_test: int, num_features: int=2, noise: float=0.0):
@@ -162,6 +162,40 @@ def generate_gaussian_quantile_dataset(n_train: int, n_update: int, n_test: int,
                                      n_classes=2)
     x = np.concatenate((X1, X2))
     y = np.concatenate((y1, - y2 + 1))
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=n_update + n_test)
+    x_update, x_test, y_update, y_test = train_test_split(x_test, y_test, test_size=n_test)
+
+    return x_train, y_train, x_update, y_update, x_test, y_test
+
+
+def generate_s_shape_dataset(n_train: int, n_update: int, n_test: int, num_features: int=2, noise: float=0.0) -> Tuple:
+    total_samples = (n_train + n_update + n_test) // 2
+    y = np.linspace(2.0, 3.0, total_samples)
+    x_0 = 0.25 * ((np.cos(3.5 * y) + np.random.normal(0, 0.1, len(y))) + 4) + 1.9
+    x_1 = 0.25 * ((np.cos(3.5 * y) + np.random.normal(0, 0.1, len(y))) + 4.5) + 1.9
+
+    x = np.concatenate([np.stack([x_0, y], 1),
+                        np.stack([x_1, y], 1)])
+    y = np.concatenate([np.zeros(total_samples),
+                       np.ones(total_samples)])
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=n_update + n_test)
+    x_update, x_test, y_update, y_test = train_test_split(x_test, y_test, test_size=n_test)
+
+    return x_train, y_train, x_update, y_update, x_test, y_test
+
+
+def generate_long_s_shape_dataset(n_train: int, n_update: int, n_test: int, num_features: int=2, noise: float=0.0) -> Tuple:
+    total_samples = (n_train + n_update + n_test) // 2
+    y = np.linspace(2.0, 5.0, total_samples)
+    x_0 = 0.25 * ((np.cos(3.5 * y) + np.random.normal(0, 0.1, len(y))) + 4) + 1.9
+    x_1 = 0.25 * ((np.cos(3.5 * y) + np.random.normal(0, 0.1, len(y))) + 4.5) + 1.9
+
+    x = np.concatenate([np.stack([x_0, y], 1),
+                        np.stack([x_1, y], 1)])
+    y = np.concatenate([np.zeros(total_samples),
+                       np.ones(total_samples)])
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=n_update + n_test)
     x_update, x_test, y_update, y_test = train_test_split(x_test, y_test, test_size=n_test)
