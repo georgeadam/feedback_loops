9baa4bd7661e2af0cde981170fd4a0225646de43
alexadam <paddze21@gmail.com>
2021-01-08 17:09:31 -0500

    Modified moons dataset with custom version
    -Custom version allows wraparound such that we would have
    high confidence FPs

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,11 +1,17 @@
 import copy
+import numbers
 import numpy as np
 import os
 import pandas as pd
 from scipy.stats import multivariate_normal
 
-from sklearn.datasets import make_classification, make_gaussian_quantiles, make_moons
+from sklearn.datasets import make_classification, make_gaussian_quantiles
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.utils import check_array, check_random_state
+from sklearn.utils import shuffle as util_shuffle
+from sklearn.utils.random import sample_without_replacement
+from sklearn.utils.validation import _deprecate_positional_args
 from src.utils.typing import DataFn
 
 from omegaconf import DictConfig
@@ -39,7 +45,7 @@ def get_data_fn(d: DictConfig, m: DictConfig) -> DataFn:
                                      mimic_iv_paths[d.type]["path"], d.balanced, temporal=d.temporal,
                                      categorical=mimic_iv_paths[d.type]["categorical"], model=m.type)
     elif d.type == "moons":
-        return generate_moons_dataset
+        return generate_moons_dataset(d.start, d.end, d.noise)
     elif d.type == "support2":
         return generate_real_dataset(load_support2cls_data)
 
@@ -78,6 +84,64 @@ def make_gaussian_data(m0: float, m1: float, s0: float, s1: float, n: int, p0: f
     y = y[idx]
 
     return x, y
+
+
+def make_moons(n_samples=100, *, start=0.0, end=np.pi, shuffle=True, noise=None, random_state=None):
+    """Make two interleaving half circles.
+       A simple toy dataset to visualize clustering and classification
+       algorithms. Read more in the :ref:`User Guide <sample_generators>`.
+       Parameters
+       ----------
+       n_samples : int or tuple of shape (2,), dtype=int, default=100
+           If int, the total number of points generated.
+           If two-element tuple, number of points in each of two moons.
+           .. versionchanged:: 0.23
+              Added two-element tuple.
+       shuffle : bool, default=True
+           Whether to shuffle the samples.
+       noise : float, default=None
+           Standard deviation of Gaussian noise added to the data.
+       random_state : int, RandomState instance or None, default=None
+           Determines random number generation for dataset shuffling and noise.
+           Pass an int for reproducible output across multiple function calls.
+           See :term:`Glossary <random_state>`.
+       Returns
+       -------
+       X : ndarray of shape (n_samples, 2)
+           The generated samples.
+       y : ndarray of shape (n_samples,)
+           The integer labels (0 or 1) for class membership of each sample.
+       """
+
+    if isinstance(n_samples, numbers.Integral):
+        n_samples_out = n_samples // 2
+        n_samples_in = n_samples - n_samples_out
+    else:
+        try:
+            n_samples_out, n_samples_in = n_samples
+        except ValueError as e:
+            raise ValueError('`n_samples` can be either an int or '
+                             'a two-element tuple.') from e
+
+    generator = check_random_state(random_state)
+
+    outer_circ_x = np.cos(np.linspace(0, np.pi, n_samples_out))
+    outer_circ_y = np.sin(np.linspace(0, np.pi, n_samples_out))
+    inner_circ_x = 1 - np.cos(np.linspace(start, end, n_samples_in))
+    inner_circ_y = 1 - np.sin(np.linspace(start, end, n_samples_in)) - .5
+
+    X = np.vstack([np.append(outer_circ_x, inner_circ_x),
+                   np.append(outer_circ_y, inner_circ_y)]).T
+    y = np.hstack([np.ones(n_samples_out, dtype=np.intp),
+                   np.zeros(n_samples_in, dtype=np.intp)])
+
+    if shuffle:
+        X, y = util_shuffle(X, y, random_state=generator)
+
+    if noise is not None:
+        X += generator.normal(scale=noise, size=X.shape)
+
+    return X, y
 
 
 def make_trend_gaussian_data(m0: float, m1: float, s0: float, s1: float, n: int, num_features: int=2, noise: float=0.0,
@@ -134,13 +198,14 @@ def generate_sklearn_make_classification_dataset(noise: float=0.0) -> Callable:
 
     return wrapped
 
-def generate_moons_dataset(noise: float=0.0) -> Callable:
-    def wrapped(n_train: int, n_update: int, n_test: int, num_features: int=2) -> Tuple:
-        x_train, y_train = make_moons(n_train, noise=noise)
-        x_update, y_update = make_moons(n_update, noise=noise)
-        x_test, y_test = make_moons(n_test, noise=noise)
 
-        return x_train, y_train, x_update, y_update, x_test, y_test
+def generate_moons_dataset(start: float=0.0, end: float=np.pi, noise: float=0.0) -> Callable:
+    def wrapped(n_train: int, n_update: int, n_test: int, num_features: int=2) -> Tuple:
+        x_train, y_train = make_moons(n_train, start=0.0, end=np.pi, noise=noise)
+        x_update, y_update = make_moons(n_update, start=start, end=end, noise=noise)
+        x_test, y_test = make_moons(n_test, start=0.0, end=end, noise=noise)
+
+        return x_train, y_train, x_update, y_update, x_test, y_test, None
 
     return wrapped
 
