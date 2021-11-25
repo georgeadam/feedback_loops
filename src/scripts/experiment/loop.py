f1e1522cd6ff27eae2dc380522242cc8363378f2
alexadam <paddze21@gmail.com>
2021-11-25 14:34:19 -0500

    Renamed loops and experiment formatting files

--- src/scripts/experiment/loop.py
+++ src/scripts/experiment/loop.py
@@ -5,7 +5,7 @@ from src.utils.metrics import compute_all_rates, RateTracker
 from src.utils.misc import create_empty_rates
 from src.utils.preprocess import get_scaler
 from src.utils.rand import set_seed
-from src.utils.threshold import find_threshold
+from src.scripts.experiment.threshold import find_threshold
 from src.utils.typing import ResultDict, ModelFn, DataFn
 
 from omegaconf import DictConfig
@@ -23,8 +23,7 @@ def get_dyanmic_desired_value(ddr: str, rates: Dict[str, List[float]]) -> Option
 
 def experiment_loop(data_fn: DataFn=None, data_wrapper_fn=None, model_fn: ModelFn=None, trainer_fn=None,
                     update_fn: Callable=None, idr: str= "fpr", idv: float=0.1,
-                    seeds: int=1, clinician_fpr: float=0.0, clinician_trust: float=1.0,
-                    normalize: bool=True, recover_prob: float=1.0, return_model: bool=False, **kwargs) -> (ResultDict, List):
+                    seeds: int=1, normalize: bool=True, return_model: bool=False, **kwargs) -> (ResultDict, List):
     seeds = np.arange(seeds)
     rates = create_empty_rates()
     models = []
@@ -67,8 +66,7 @@ def experiment_loop(data_fn: DataFn=None, data_wrapper_fn=None, model_fn: ModelF
 
         # Update model
         model = update_fn(model, data_wrapper, rate_tracker, trainer=trainer,
-                          ddv=ddv, clinician_fpr=clinician_fpr, clinician_trust=clinician_trust,
-                          scaler=scaler, recover_prob=recover_prob)
+                          ddv=ddv, scaler=scaler)
         temp_rates = rate_tracker.get_rates()
 
         if return_model:
@@ -80,10 +78,8 @@ def experiment_loop(data_fn: DataFn=None, data_wrapper_fn=None, model_fn: ModelF
     return rates, models
 
 
-def call_update_loop(args: DictConfig, data_fn: Callable, data_wrapper_fn: Callable, model_fn: Callable,
-                     trainer, update_fn: Callable) -> (ResultDict, ResultDict):
+def call_experiment_loop(args: DictConfig, data_fn: Callable, data_wrapper_fn: Callable, model_fn: Callable,
+                         trainer, update_fn: Callable) -> (ResultDict, ResultDict):
     return experiment_loop(data_fn, data_wrapper_fn, model_fn, trainer, update_fn,
                            idr=args.rates.idr, idv=args.rates.idv, seeds=args.misc.seeds,
-                           clinician_fpr=args.rates.clinician_fpr,
-                           clinician_trust=args.trust.clinician_trust, normalize=args.data.normalize,
-                           recover_prob=args.rates.recover_prob, return_model=args.misc.return_model)
+                           normalize=args.data.normalize, return_model=args.misc.return_model)
